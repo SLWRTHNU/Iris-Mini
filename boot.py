520a719a26bc4f49cb2edcb61f36176835a697c6
SLWRTHNU <kyle.e.beaulieu+github@gmail.com>
2026-04-21 11:18:18 -0400

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,5 +1,7 @@
 # boot.py - Iris Mini ESP32-S3
 # Runs on every boot, before main.py
-
+from machine import Pin
+p = Pin(38, Pin.OUT, value=1)
 print("Iris Mini - booting")
 # Factory reset is handled at runtime via task_factory_reset_button in app_main.py
+
